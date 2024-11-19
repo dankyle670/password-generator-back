bc5dcd9f20c684c30a520a67cb428b91b0d47769
daniel komoe <daniel.komoe@epitech.eu>
2024-11-19 13:22:27 +0100

    [ADD] require body-=parser in the server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const bcrypt = require('bcrypt');
